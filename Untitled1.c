da0e9d063e2cec76adcf94ec49543cbc63333902
victor lisboa <165725972+victorlisb@users.noreply.github.com>
2024-07-28 15:35:58 -0300

    Update Untitled1.c
    Retirei comentarios

--- Untitled1.c
+++ Untitled1.c
@@ -11,16 +11,5 @@ void main()
            printf("%d \n",i);
        }
     }
-    printf("A soma de todos os numeros pares entre 1 e 100 é %d",soma);
-
-    //sequencia de fibonacci
-
-    /*int i;
-
-    for(i=0;i<100;i++)
-    {
-        auxiliar=(auxiliar-1)+()
-        printf("o")
-    } */
-
+    printf("A soma de todos os numeros pares entre 1 e 100 Ã© %d",soma);
 }
